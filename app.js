dd1e3622480c155b6d66f096d32125828c67420a
Joaquin <joaquin.alonso.astudillo@gmail.com>
2021-10-31 11:20:25 +0000

    update log message for request

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const axios = require("axios");
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
     signingSecret: process.env.SLACK_SIGNING_SECRET,
-    socketMode: true, 
+    socketMode: true,
     appToken: process.env.SLACK_APP_TOKEN
 });
 
@@ -19,12 +19,13 @@ const checkAppointments = () => {
             if (!isEmpty) {
                 sendMessage("Appointment found! <https://burghquayregistrationoffice.inis.gov.ie/Website/AMSREG/AMSRegWeb.nsf/AppSelect?OpenForm|Click *here* to schedule>");
             }
-        } 
+            console.log(data);
+        }
         catch (error) {
             console.error(error);
             sendMessage(error);
         }
-        
+
     }, 900000);
 };
 
@@ -38,18 +39,18 @@ const sendMessage = async (message) => {
     try {
         const channelId = process.env.SLACK_CHANNEL_ID;
         const result = await app.client.chat.postMessage({
-          channel: channelId,
-          text: message,
+            channel: channelId,
+            text: message,
         });
-      
+
         console.log(result);
-      }
+    }
     catch (error) {
         console.error(error);
     }
 };
 
-startApp(); 
+startApp();
 
 
 
